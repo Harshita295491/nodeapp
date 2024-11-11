dae415d2fb03254983c9d3b9f86187f175adf636
Harshita295491 <harshitha.pathakamuri@thebluespire.com>
2024-11-11 12:44:06 +0530

    first commit

---
+++ tests/app.test.js
@@ -0,0 +1,10 @@
+describe("sample tests",()=>{
+    test("first test case",()=>{
+        expect(1+1).toBe(2);
+    });
+
+    test("second test case", ()=>{
+        expect(1+2).toBe(3)
+
+    })
+});
